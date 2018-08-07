8c3a7b273d1f1288e974b7079530906b9dfd2bcd
cici <cici@arpnetwork.org>
2018-08-07 16:21:25 +0800

    Add gas estimate

--- app/src/main/java/org/arpnetwork/arpdevice/contracts/ARPContract.java
+++ app/src/main/java/org/arpnetwork/arpdevice/contracts/ARPContract.java
@@ -1,9 +1,12 @@
 package org.arpnetwork.arpdevice.contracts;
 
 import org.arpnetwork.arpdevice.contracts.api.BalanceAPI;
-import org.arpnetwork.arpdevice.contracts.api.VerifyAPI;
+import org.arpnetwork.arpdevice.contracts.api.TransactionAPI;
 import org.arpnetwork.arpdevice.contracts.tasks.ARPAllowanceTask;
+import org.arpnetwork.arpdevice.contracts.tasks.BindMinerHelper;
 import org.arpnetwork.arpdevice.contracts.tasks.OnValueResult;
+import org.arpnetwork.arpdevice.contracts.tasks.TransactionGasEstimateTask;
+import org.arpnetwork.arpdevice.ui.wallet.WalletManager;
 import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
@@ -13,6 +16,7 @@ import org.web3j.abi.datatypes.Type;
 import org.web3j.abi.datatypes.Uint;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.tx.Contract;
 import org.web3j.utils.Convert;
 
@@ -32,10 +36,11 @@ public class ARPContract extends Contract {
         return new ARPContract(web3j, credentials, gasPrice, gasLimit);
     }
 
-    public String getApproveFunctionData(String spenderAddress, BigInteger value) {
+    public static String getApproveFunctionData(String spenderAddress, BigInteger value) {
         Function function = new Function("approve",
                 Arrays.<Type>asList(new Address(spenderAddress), new Uint(value)),
-                Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
+                Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {
+                }));
         String encodedFunction = FunctionEncoder.encode(function);
         return encodedFunction;
     }
@@ -43,7 +48,7 @@ public class ARPContract extends Contract {
     public static String getTransactionHexData(String address, Credentials credentials,
             BigInteger gasPrice, BigInteger gasLimit) {
         ARPContract contract = ARPContract.load(BalanceAPI.getWeb3J(), credentials, gasPrice, gasLimit);
-        String hexData = VerifyAPI.getRawTransaction(gasPrice, gasLimit, ARPContract.CONTRACT_ADDRESS,
+        String hexData = TransactionAPI.getRawTransaction(gasPrice, gasLimit, CONTRACT_ADDRESS,
                 contract.getApproveFunctionData(address, new BigInteger(Convert.toWei("500", Convert.Unit.ETHER).toString())), credentials);
         return hexData;
     }
@@ -51,5 +56,18 @@ public class ARPContract extends Contract {
     public static void allowanceARP(String owner, String spender, OnValueResult<BigDecimal> onResult) {
         ARPAllowanceTask arpAllowanceTask = new ARPAllowanceTask(onResult);
         arpAllowanceTask.execute(owner, spender);
+    }
+
+    public static void getApproveEstimateGas(OnValueResult<BigInteger> onValueResult) {
+        String ownerAddress = WalletManager.getInstance().getWallet().getPublicKey();
+        String spenderAddress = BindMinerHelper.CONTRACT_ADDRESS;
+        BigInteger value = new BigInteger(Convert.toWei("50000", Convert.Unit.ETHER).toString());
+        String data = getApproveFunctionData(spenderAddress, value);
+
+        Transaction approveTransaction = new Transaction(ownerAddress, new BigInteger("0"),
+                new BigInteger("10000000"), new BigInteger("10000000"), CONTRACT_ADDRESS, new BigInteger("0"), data);
+
+        TransactionGasEstimateTask gasEstimateTask = new TransactionGasEstimateTask(approveTransaction, onValueResult);
+        gasEstimateTask.execute();
     }
 }
---
+++ app/src/main/java/org/arpnetwork/arpdevice/contracts/api/TransactionAPI.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 ARP Network
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arpnetwork.arpdevice.contracts.api;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthEstimateGas;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.utils.Numeric;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class TransactionAPI {
+
+    /**
+     * Get row transaction string by params
+     *
+     * @param gasPrice
+     * @param gasLimit
+     * @param contractAddress
+     * @param data
+     * @param credentials
+     * @return
+     */
+    public static String getRawTransaction(BigInteger gasPrice, BigInteger gasLimit,
+            String contractAddress, String data, Credentials credentials) {
+        RawTransaction transaction = getTransaction(gasPrice, gasLimit, contractAddress, data, credentials);
+        byte[] signedMessage = TransactionEncoder.signMessage(transaction, credentials);
+        String hexValue = Numeric.toHexString(signedMessage);
+        return hexValue;
+    }
+
+    public static BigInteger getTransactionGasLimit(Transaction transaction) throws ExecutionException, InterruptedException {
+        Future<EthEstimateGas> gas = BalanceAPI.getWeb3J().ethEstimateGas(transaction).sendAsync();
+        return gas.get().getAmountUsed();
+    }
+
+    private static BigInteger getTransactionCount(String address) {
+        EthGetTransactionCount transactionCount = new EthGetTransactionCount();
+        try {
+            transactionCount = BalanceAPI.getWeb3J()
+                    .ethGetTransactionCount(address, DefaultBlockParameterName.PENDING).send();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return transactionCount.getTransactionCount();
+    }
+
+    private static RawTransaction getTransaction(BigInteger gasPrice, BigInteger gasLimit,
+            String contractAddress, String data, Credentials credentials) {
+        BigInteger nonce = getTransactionCount(credentials.getAddress());
+        RawTransaction rawTransaction  = RawTransaction.createTransaction(
+                nonce, gasPrice, gasLimit, contractAddress, data);
+        return rawTransaction;
+    }
+}
