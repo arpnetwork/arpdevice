f8b7bdf0506a2d45721bea8e829d5dccce0d47fd
arvinharf <arvin@arpnetwork.org>
2018-08-28 15:45:32 +0800

    Fix an issue of http server receiving data error

--- app/src/main/java/org/arpnetwork/arpdevice/server/http/HttpServerHandler.java
+++ app/src/main/java/org/arpnetwork/arpdevice/server/http/HttpServerHandler.java
@@ -23,10 +23,13 @@ import java.net.InetSocketAddress;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelId;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
@@ -46,62 +49,76 @@ class HttpServerHandler extends ChannelInboundHandlerAdapter {
     private static final String CONNECTION = "connection";
 
     private Dispatcher mDispatcher;
-    private HttpMethod mMethod;
-    private String mUri;
+    private ConcurrentMap<ChannelId, RequestData> mRequestDataMap;
 
     public HttpServerHandler(Dispatcher dispatcher) {
         super();
 
         mDispatcher = dispatcher;
+        mRequestDataMap = new ConcurrentHashMap<>();
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ChannelId channelId = ctx.channel().id();
+        RequestData requestData = mRequestDataMap.get(channelId);
+        if (requestData == null) {
+            requestData = new RequestData();
+            requestData.strBuffer = new StringBuffer();
+            mRequestDataMap.put(channelId, requestData);
+        }
+
         if (msg instanceof HttpRequest) {
             HttpRequest httpRequest = (HttpRequest) msg;
-            mUri = httpRequest.uri();
-            mMethod = httpRequest.method();
+            requestData.uri = httpRequest.uri();
+            requestData.method = httpRequest.method();
         }
 
         if (msg instanceof HttpContent) {
-            Request request = new Request();
-            request.setMethod(mMethod);
-            request.setUri(mUri);
             if (ctx.channel().remoteAddress() instanceof InetSocketAddress) {
                 InetSocketAddress socketAddress = (InetSocketAddress) ctx.channel().remoteAddress();
-                request.setRemoteAddress(socketAddress.getAddress().getHostAddress());
+                requestData.remoteAddress = socketAddress.getAddress().getHostAddress();
             }
 
-            if (mMethod == HttpMethod.GET) {
-                request.setContent(getRequestParams(mUri));
+            if (requestData.method == HttpMethod.GET) {
+                String content = getRequestParams(requestData.uri);
+                requestData.strBuffer.append(content);
             }
 
-            if (mMethod == HttpMethod.POST) {
-                HttpContent httpContent = (HttpContent) msg;
-                ByteBuf content = httpContent.content();
-                byte[] data = new byte[content.readableBytes()];
-                content.readBytes(data);
-                content.release();
+            if (requestData.method == HttpMethod.POST) {
+                ByteBuf buf = ((HttpContent) msg).content();
+                byte[] data = new byte[buf.readableBytes()];
+                buf.readBytes(data);
+                buf.release();
 
-                request.setContent(new String(data));
+                String content = new String(data);
+                requestData.strBuffer.append(content);
             }
-
-            Response response = new Response();
-
-            mDispatcher.service(request, response);
-
-            FullHttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
-                    HttpResponseStatus.valueOf(response.getStatus()), Unpooled.wrappedBuffer(response.getContent().getBytes()));
-            httpResponse.headers().set(CONTENT_TYPE, response.getContentType());
-            httpResponse.headers().set(CONTENT_LENGTH, httpResponse.content().readableBytes());
-            httpResponse.headers().set(CONNECTION, HttpHeaderValues.KEEP_ALIVE);
-            ctx.write(httpResponse);
-            ctx.flush();
         }
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
+        ChannelId channelId = ctx.channel().id();
+        RequestData requestData = mRequestDataMap.get(channelId);
+        mRequestDataMap.remove(channelId);
+
+        Request request = new Request();
+        request.setMethod(requestData.method);
+        request.setUri(requestData.uri);
+        request.setRemoteAddress(requestData.remoteAddress);
+        request.setContent(requestData.strBuffer.toString());
+
+        Response response = new Response();
+
+        mDispatcher.service(request, response);
+
+        FullHttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                HttpResponseStatus.valueOf(response.getStatus()), Unpooled.wrappedBuffer(response.getContent().getBytes()));
+        httpResponse.headers().set(CONTENT_TYPE, response.getContentType());
+        httpResponse.headers().set(CONTENT_LENGTH, httpResponse.content().readableBytes());
+        httpResponse.headers().set(CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        ctx.write(httpResponse);
         ctx.flush();
     }
 
@@ -120,5 +137,12 @@ class HttpServerHandler extends ChannelInboundHandlerAdapter {
             jsonObject.put(entry.getKey(), entry.getValue().get(0));
         }
         return jsonObject.toString();
+    }
+
+    private class RequestData {
+        HttpMethod method;
+        String uri;
+        String remoteAddress;
+        StringBuffer strBuffer;
     }
 }
